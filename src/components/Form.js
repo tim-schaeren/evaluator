ec8220b5623d2830b1a1e9663e9cc020466b27fb
tim-schaeren <tim.schaeren@gmail.com>
2022-01-20 10:44:19 +0100

    expanded on the input form

--- src/components/Form.js
+++ src/components/Form.js
@@ -21,39 +21,95 @@
 
      return (
          <form className='add-form' onSubmit={onSubmit}>
-            <div className=''>
-                <label>How many product categories do you have?</label>
+
+<br></br>
+
+            <div className='question'>
+                <label>What's the product you want to think about today?</label>
             </div>
+
+            <div className='form-control'>
+                <input type='text' placeholder='enter product name' value={day} onChange={(e) => setDay(e.target.value)} />
+             </div>
+
+            <br></br>
+
+            <div className='question'>
+                <label>Into which market do you plan to expand?</label>
+            </div>
+
+            <div className='form-control'>
+                <input type='text' placeholder='enter name of market' value={day} onChange={(e) => setDay(e.target.value)} />
+             </div>
+
+            <br></br>
+
+            <label>Use the sliders to indicate how much you agree</label>
+
+            <br></br>
+            <br></br>
+
+            <div className='question'>
+                <label>What's your plan on how to succeed in the new market?</label>
+            </div>
+
+            <br></br>
+
+            <label>Aggregation</label>
             <div className='form-control form-control-check'>
-                <label htmlFor="justOne">just one</label>
-                <input type='checkbox' id='justOne' value={text} onChange={(e) => setText(e.target.value)} />
-               
-                <label htmlFor="aFew">a few</label>
-                <input type='checkbox' id='aFew' value={text} onChange={(e) => setText(e.target.value)} />
-                
-                <label htmlFor="lots">lots</label>
-                <input type='checkbox' id='lots' value={text} onChange={(e) => setText(e.target.value)} />
-           </div>
-           <div className='form-control'>
-               <label>What's the product you want to think about today?</label>
-               <input type='text' placeholder='product name' value={day} onChange={(e) => setDay(e.target.value)} />
-           </div>
-           <div className='form-control form-control-check'>
-               <label>Choose strategy option A</label>
-               <input type='checkbox' checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
-           </div>
-           <div className='form-control form-control-check'>
-               <label>Choose strategy option B</label>
-               <input type='checkbox'/>
-           </div>
-           <div className='form-control form-control-check'>
-               <label>Choose strategy option C</label>
-               <input type='checkbox'/>
-           </div>
-           <div className='form-control form-control-check'>
-               <label>Choose strategy option D</label>
-               <input type='checkbox'/>
-           </div>
+               <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>Arbitrage</label>
+            <div className='form-control form-control-check'>
+               <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>Adaption</label>
+            <div className='form-control form-control-check'>
+               <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <br></br>
+
+            <div className='question'>
+                <label>How do you plan on achieving this?</label>
+            </div>
+
+            <br></br>
+
+            <label>We are ready to make changes to our product</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>We don't require a minimum number of sales</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>We aim to standardize our product</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>Lever 4</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>Lever 5</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+            <label>Lever 6</label>
+            <div className='form-control form-control-check'>
+            <input type='range' min="0" max="10" defaultValue="0" onChange={(e) => setReminder(e.currentTarget.checked)} />
+            </div>
+
+
+
 
            <input type='submit' value='Evaluate!' className='btn btn-block'/>
          </form>
